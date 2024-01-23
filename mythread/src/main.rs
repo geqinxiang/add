f71b88525e8d8905e72d2e0720f27ebb20432b7b
geqinxiang <1255940707@qq.com>
2024-01-23 16:31:55 +0800

    xiancheng

--- mythread/src/main.rs
+++ mythread/src/main.rs
@@ -1,4 +1,8 @@
-use std::thread;
+use std::rc::Rc;
+use std::sync::{Arc, mpsc, Mutex};
+use std::sync::atomic::{AtomicUsize,Ordering};
+use std::{hint, thread};
+use std::ops::Deref;
 use std::time::Duration;
 
 fn main() {
@@ -31,4 +35,83 @@ fn main() {
     // drop(v); //在这里会报错handle3结束前不能被析构
     handle3.join().unwrap();
     // drop(v); // ok
+    //线程间的消息传递
+    // 使用 mpsc::channel 函数创建一个新的信道；mpsc 是 多个生产者，单个消费者（multiple producer, single consumer）的缩写。
+    let (tx, rx) = mpsc::channel();
+    // let (tx1, rx1) = mpsc::channel();
+    thread::spawn(move || {
+        // tx1.send( String::from("hi======")).unwrap();
+        let val = String::from("hi----");
+        tx.send(val).unwrap();
+        //下面这段代码对线程间的消息传递没有意义，只是想看两个线程是并行的
+        for i in 1..5 {
+            println!("hi   number {} from the spawned333333333 thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+
+    });
+    // let rec=rx1.try_recv().unwrap();
+    // println!("Got: {}", rec);
+    let received = rx.recv().unwrap();
+    println!("Got11111: {}", received);
+    //线程间的消息传递
+    //实现多生产者 一个消费者，通过指针克隆
+    let (tx2, rx2) = mpsc::channel();
+    let tx21 = tx2.clone();//在创建新线程之前，我们对发送者调用了 clone 方法。这会给我们一个可以传递给第一个新建线程的发送端句柄。
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("22more"),
+            String::from("22messages"),
+            String::from("22for"),
+            String::from("22you"),
+        ];
+
+        for val in vals {
+            tx21.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx2 {
+        println!("Got======: {}", received);
+    }
+//     线程间消息共享 多线程和多所有权
+//     通过 Arc实现多所有权
+// Arc<T> 正是 这么一个类似 Rc<T> 并可以安全的用于并发环境的类型
+//     Arc::clone 重新定义的是指针，直指指向的区域没有变化 通过此实现多所有权
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for i in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+            println!("--------::{}::{}",i,&num);
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+
+
+
 }
